26f1920589090ef5adb1f162f83b811d6cc23e65
korzhn066@gmail.com <korzhn066@gmail.com>
2023-09-28 14:26:32 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _db;
+        public OrderService(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
+
+        public async Task<Order> GetOrder()
+        {
+            var maxPrice = await _db.Orders.MaxAsync(o => o.Price * o.Quantity);
+
+            return await _db.Orders.FirstAsync(o => o.Price * o.Quantity == maxPrice);
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            return await _db.Orders.Where(o => o.Quantity > 10).ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _db;
+        public UserService(ApplicationDbContext db) 
+        { 
+            _db = db;
+        }
+        public async Task<User> GetUser()
+        {
+            var users = await _db.Users.Include(u => u.Orders).ToListAsync();
+
+            var maxCount = 0;
+            User result = new();
+
+            foreach (var user in users)
+            {
+                if (user.Orders.Count > maxCount)
+                {
+                    maxCount = user.Orders.Count;
+                    result.Status = user.Status;
+                    result.Email = user.Email;
+                    result.Id = user.Id;
+                }
+            }
+
+
+            return result;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await _db.Users.Where(u => u.Status == UserStatus.Inactive).ToListAsync();
+        }
+    }
+}
